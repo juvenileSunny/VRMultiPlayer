9cf0eda6745d4940f6199f07ae513ca8d48e6f03
Mohammad Jahed Murad Sunny <64752337+juvenileSunny@users.noreply.github.com>
2026-02-17 15:33:00 -0600

    Final Lecture Scene is done with waypoint to mindmap

--- Assets/LectureSessionNetworkController.cs
+++ Assets/LectureSessionNetworkController.cs
@@ -1,18 +1,82 @@
-using System.Collections;
-using System.Collections.Generic;
+using Unity.Netcode;
 using UnityEngine;
+using XRMultiplayer.MiniGames;
+using System.Collections.Generic;
 
-public class LectureSessionNetworkController : MonoBehaviour
+public class LectureSessionNetworkController : NetworkBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("References")]
+    public LectureSessionManager lectureSession;
+    public SlideTTSAgent slideTTSAgent;
+
+    [Header("Objects To Deactivate On Finish")]
+    public List<GameObject> objectsToDisableOnFinish = new();
+
+    [Header("Objects To Activate For Next MiniGame")]
+    public List<GameObject> objectsToEnableOnFinish = new();
+
+    /* =========================
+     * CALLED BY HOST BUTTON
+     * ========================= */
+    public void RequestFinishSession()
     {
-        
+        if (!IsServer)
+            RequestFinishSessionServerRpc();
+        else
+            FinishSession();
     }
 
-    // Update is called once per frame
-    void Update()
+    [ServerRpc(RequireOwnership = false)]
+    void RequestFinishSessionServerRpc()
     {
-        
+        FinishSession();
+    }
+
+    /* =========================
+     * SERVER LOGIC
+     * ========================= */
+    void FinishSession()
+    {
+        // Stop TTS on server
+        if (slideTTSAgent != null)
+            slideTTSAgent.StopSpeaking();
+
+        // Deactivate lecture objects
+        foreach (var obj in objectsToDisableOnFinish)
+            if (obj != null)
+                obj.SetActive(false);
+
+        // Activate next minigame objects
+        foreach (var obj in objectsToEnableOnFinish)
+            if (obj != null)
+                obj.SetActive(true);
+
+        // Finish lecture logic
+        if (lectureSession != null)
+            lectureSession.FinishGame();
+
+        // Sync to all clients
+        FinishSessionClientRpc();
+    }
+
+    /* =========================
+     * CLIENT SIDE
+     * ========================= */
+    [ClientRpc]
+    void FinishSessionClientRpc()
+    {
+        if (slideTTSAgent != null)
+            slideTTSAgent.StopSpeaking();
+
+        foreach (var obj in objectsToDisableOnFinish)
+            if (obj != null)
+                obj.SetActive(false);
+
+        foreach (var obj in objectsToEnableOnFinish)
+            if (obj != null)
+                obj.SetActive(true);
+
+        if (lectureSession != null)
+            lectureSession.FinishGame();
     }
 }
