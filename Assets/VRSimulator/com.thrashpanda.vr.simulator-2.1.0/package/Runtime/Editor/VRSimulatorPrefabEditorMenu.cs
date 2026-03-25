3f39fc63240a02552aef94ae69498fd9ee0b125d
Mohammad Jahed Murad Sunny <64752337+juvenileSunny@users.noreply.github.com>
2026-03-25 02:10:34 -0500

    PC and VR asymmetric setup without build artifacts

---
+++ Assets/VRSimulator/com.thrashpanda.vr.simulator-2.1.0/package/Runtime/Editor/VRSimulatorPrefabEditorMenu.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+#if USE_INTERACTION_TOOLKIT
+using UnityEngine.XR.Interaction.Toolkit;
+#elif USE_INTERACTION_TOOLKIT_2
+using Unity.XR.CoreUtils;
+#endif
+
+[InitializeOnLoad]
+public static class VRSimulatorPrefabEditorMenu
+{
+    static string packageName = "com.thrashpanda.vr.simulator";
+
+    [MenuItem("GameObject/XR/VR Simulator"), MenuItem("VRSimulator/Create/VR Simulator")]
+    public static void InstantiateVRSimulatorInScene()
+    {
+        Object prefab = AssetDatabase.LoadAssetAtPath("Packages/" + packageName + "/Runtime/Assets/Prefab/VRSimulator.prefab", typeof(GameObject));
+        GameObject simulator = (GameObject)PrefabUtility.InstantiatePrefab(prefab);
+#if USE_INTERACTION_TOOLKIT
+        Debug.Log("Unity Interaction Toolkit <2 detected, trying to find XRRig in Scene.");
+        XRRig xRRig;
+        if (xRRig = GameObject.FindObjectOfType<XRRig>())
+            simulator.GetComponent<VRSimulator>().xRRigToFollow = xRRig.transform;
+#elif USE_INTERACTION_TOOLKIT_2
+        Debug.Log("Unity Interaction Toolkit >2 detected, trying to find XROrigin in Scene.");
+        XROrigin xRRig;
+        if (xRRig = GameObject.FindObjectOfType<XROrigin>())
+            simulator.GetComponent<VRSimulator>().xRRigToFollow = xRRig.transform;
+#endif
+    }
+}
---
+++ Assets/XRInputModeSwitcher.cs
@@ -0,0 +1,100 @@
+using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.InputSystem;
+using UnityEngine.XR.Interaction.Toolkit.Inputs;
+using UnityEngine.XR.Interaction.Toolkit.Inputs.Simulation;
+
+public class XRInputModeSwitcher : MonoBehaviour
+{
+    [Header("References")]
+    public GameObject xrDeviceSimulatorObject;
+    public InputActionManager inputActionManager;
+
+    [Header("Action Assets")]
+    public InputActionAsset vrInputActions;
+    public InputActionAsset pcSimulatorActions;
+    public InputActionAsset pcControllerActions;
+
+    [Header("Optional components to disable in PC mode")]
+    public Behaviour[] vrOnlyComponents;
+
+    [Header("Optional components to disable in VR mode")]
+    public Behaviour[] pcOnlyComponents;
+
+    [Header("Debug Override")]
+    public bool forcePCMode = false;
+    public bool forceVRMode = false;
+
+    void Start()
+    {
+        bool isVR;
+
+        if (forcePCMode)
+            isVR = false;
+        else if (forceVRMode)
+            isVR = true;
+        else
+            isVR = XRSettings.isDeviceActive;
+
+        if (isVR)
+            EnableVRMode();
+        else
+            EnablePCMode();
+    }
+
+    void EnableVRMode()
+    {
+        Debug.Log("XRInputModeSwitcher: VR mode enabled");
+
+        if (xrDeviceSimulatorObject != null)
+            xrDeviceSimulatorObject.SetActive(false);
+
+        if (vrInputActions != null) vrInputActions.Enable();
+        if (pcSimulatorActions != null) pcSimulatorActions.Disable();
+        if (pcControllerActions != null) pcControllerActions.Disable();
+
+        if (vrOnlyComponents != null)
+        {
+            foreach (var c in vrOnlyComponents)
+            {
+                if (c != null) c.enabled = true;
+            }
+        }
+
+        if (pcOnlyComponents != null)
+        {
+            foreach (var c in pcOnlyComponents)
+            {
+                if (c != null) c.enabled = false;
+            }
+        }
+    }
+
+    void EnablePCMode()
+    {
+        Debug.Log("XRInputModeSwitcher: PC mode enabled");
+
+        if (xrDeviceSimulatorObject != null)
+            xrDeviceSimulatorObject.SetActive(true);
+
+        if (vrInputActions != null) vrInputActions.Disable();
+        if (pcSimulatorActions != null) pcSimulatorActions.Enable();
+        if (pcControllerActions != null) pcControllerActions.Enable();
+
+        if (vrOnlyComponents != null)
+        {
+            foreach (var c in vrOnlyComponents)
+            {
+                if (c != null) c.enabled = false;
+            }
+        }
+
+        if (pcOnlyComponents != null)
+        {
+            foreach (var c in pcOnlyComponents)
+            {
+                if (c != null) c.enabled = true;
+            }
+        }
+    }
+}
