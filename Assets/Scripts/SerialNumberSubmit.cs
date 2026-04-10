210f3077f78ab7f23f00b6289a2ca59f4d532763
Atit Kharel <atit286@gmail.com>
2026-04-09 19:47:23 -0500

    Add skip tutorial functionality and validation for serial number input

--- Assets/Scripts/SerialNumberSubmit.cs
+++ Assets/Scripts/SerialNumberSubmit.cs
@@ -8,21 +8,45 @@ public class SerialNumberSubmit : MonoBehaviour
 {
     public TMP_InputField serialNumberInput;
     public Button submitButton;
+    public Button skipTutorialButton;
     public string nextSceneName;
+    public string lectureSceneName;
 
     private void Start()
     {
         submitButton.onClick.AddListener(SubmitSerialNumber);
+        skipTutorialButton.onClick.AddListener(SkipTutorial);
     }
 
     private void SubmitSerialNumber()
     {
         string serialNumber = serialNumberInput.text;
+        if (string.IsNullOrEmpty(serialNumber))
+        {
+            Debug.LogWarning("Serial Number cannot be empty.");
+            return;
+        }
         DataEcho.SessionCollector.SetInstanceSerialNumber(serialNumber);
         Debug.Log("Saved Serial Number: " + serialNumber);
         DataEcho.SessionCollector.Instance.loggingData = true;
         Debug.Log("Started Logging Data with Serial Number: " + serialNumber);
         // change the scene
         UnityEngine.SceneManagement.SceneManager.LoadScene(nextSceneName);
+    }
+
+    private void SkipTutorial()
+    {
+        string serialNumber = serialNumberInput.text;
+        if (string.IsNullOrEmpty(serialNumber))
+        {
+            Debug.LogWarning("Serial Number cannot be empty.");
+            return;
+        }
+        DataEcho.SessionCollector.SetInstanceSerialNumber(serialNumber);
+        Debug.Log("Saved Serial Number: " + serialNumber);
+        DataEcho.SessionCollector.Instance.loggingData = true;
+        Debug.Log("Started Logging Data with Serial Number and Tutorial Skipped: " + serialNumber);
+        // change the scene
+        UnityEngine.SceneManagement.SceneManager.LoadScene(lectureSceneName);
     }
 }
